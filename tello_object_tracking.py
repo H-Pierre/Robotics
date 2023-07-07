00214a07b2b1a912b867fa3a56773d1110ab504e
Arnaud Aillaud <arnaud.aillaud@telecom-paris.fr>
2023-07-07 19:49:12 +0200

    Code cleanup

--- tello_object_tracking.py
+++ tello_object_tracking.py
@@ -1,19 +1,38 @@
-from utils.telloconnect import TelloConnect
-from utils.followobject import FollowObject
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import signal
-import cv2
 import argparse
+import cv2
 import pyaudio
 from text2digits import text2digits
 from transformers import pipeline
 from vosk import Model, KaldiRecognizer
+from utils.telloconnect import TelloConnect
+from utils.followobject import FollowObject
 
+
+######################### Variables globales #########################
+# Flag pour modifier le statut du mode tracking (activé / désactivé)
 is_tracking = True
+# Initialisation des composants pour la reconnaissance vocale
+Speech2Text = Model("./vosk-model-fr-0.22")
+recogniser = KaldiRecognizer(Speech2Text, 16000)
+mic = pyaudio.PyAudio()
+######################################################################
+
 
 def get_command(mic, recogniser):
+    """ Convertit un signal audio en provenance du micro en texte
+    Arguments:
+        mic: Objet pyAudio gérant l'accès au micro de l'ordinateur
+        recogniser: Modèle de Speech-to-text utilisé
+    Outputs:
+        response: string contenant le reconnu par le modèle
+    """
     listening = True
-    stream =  mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-    
+    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+
     while listening:
         stream.start_stream()
         try:
@@ -24,23 +43,34 @@ def get_command(mic, recogniser):
                 listening = False
         except OSError:
             pass
-        
+
     return response
 
+
 def translate(sentence):
+    """ Traduit une phrase française en anglais
+    Arguments:
+        sentence: String, phrase en français à traduire
+    Outputs:
+        converted_sentence: String, phrase traduite en anglais
+    """
     # model_checkpoint for French to English translation
     model_checkpoint = "Helsinki-NLP/opus-mt-fr-en"
-
     translator = pipeline("translation_fr_to_en", model=model_checkpoint)
-    
     translated_sentence = translator(sentence)[0]["translation_text"]
-
     t2d = text2digits.Text2Digits()
     converted_sentence = t2d.convert(translated_sentence)
-    
+
     return converted_sentence
 
+
 def analyze_command(command, tello):
+    """ Traduit une phrase française en anglais
+    Arguments:
+        sentence: String, phrase en français à traduire
+    Outputs:
+        converted_sentence: String, phrase traduite en anglais
+    """
     global is_tracking
     try:
         # Convert to lower case for consistency
@@ -56,12 +86,12 @@ def analyze_command(command, tello):
                 amount *= 100
             elif "millimeters" in command:
                 amount /= 10
-        
+
         if "tracking" in command:
-            if is_tracking == True:
+            if is_tracking:
                 is_tracking = False
                 print("Disabled Tracking.")
-            elif is_tracking == False:
+            elif not is_tracking:
                 is_tracking = True
                 print("Enabled Tracking.")
 
@@ -72,7 +102,7 @@ def analyze_command(command, tello):
         elif any(word in command for word in ["land"]):
             print("Landing...")
             tello.send_cmd('land')
-        
+
         elif any(word in command for word in ["up", "increased"]):
             if amount:
                 tello.send_cmd(f'up {amount}')
@@ -112,12 +142,8 @@ def analyze_command(command, tello):
     except Exception as e:
         print(f"Error in command: {e}")
 
-# Initialize speech recognition components
-Speech2Text = Model("/Users/peterharfouche/Desktop/Telecom Paris/P4/Robotique/models/vosk-model-fr-0.22")
-recogniser = KaldiRecognizer(Speech2Text, 16000)
-mic = pyaudio.PyAudio()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # input arguments
     parser = argparse.ArgumentParser(description='Tello Object tracker. keys: t-takeoff, l-land, v-video, q-quit w-up, s-down, a-ccw rotate, d-cw rotate\n')
     parser.add_argument('-model', type=str, help='DNN model caffe or tensorflow, see data folder', default='')
@@ -126,7 +152,7 @@ if __name__=="__main__":
     parser.add_argument('-dconf', type=float, help='Detection confidence, default = 0.7', default=0.7)
     parser.add_argument('-debug', type=bool, help='Enable debug, lists messages in console', default=False)
     parser.add_argument('-video', type=str, help='Use as inputs a video file, no tello needed, debug must be True', default="")
-    parser.add_argument('-vsize', type=list, help='Video size received from tello', default=(640,480))
+    parser.add_argument('-vsize', type=list, help='Video size received from tello', default=(640, 480))
     parser.add_argument('-th', type=bool, help='Horizontal tracking', default=False)
     parser.add_argument('-tv', type=bool, help='Vertical tracking', default=True)
     parser.add_argument('-td', type=bool, help='Distance tracking', default=True)
@@ -134,12 +160,10 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    #processing speed
+    # processing speed
     pspeed = 1
-
-    #img size
-    imgsize=args.vsize
-
+    # img size
+    imgsize = args.vsize
     # save video
     writevideo = False
 
@@ -157,36 +181,33 @@ if __name__=="__main__":
         tello = TelloConnect(DEBUG=False)
     tello.set_image_size(imgsize)
 
-    videow = cv2.VideoWriter('out.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (imgsize))
+    videow = cv2.VideoWriter('out.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (imgsize))
 
-    if tello.debug == True: pspeed = 30
+    if tello.debug: pspeed = 30
 
     # ask stats periodically
-    tello.add_periodic_event('wifi?',40,'Wifi')
+    tello.add_periodic_event('wifi?', 40, 'Wifi')
 
-    # wait till connected, than proceed
+    # wait till connected, then proceed
     tello.wait_till_connected()
     tello.start_communication()
     tello.start_video()
 
     fobj = FollowObject(tello, MODEL=args.model, PROTO=args.proto, CONFIDENCE=args.dconf, DEBUG=False, DETECT=args.obj)
-    fobj.set_tracking( HORIZONTAL=args.th, VERTICAL=args.tv,DISTANCE=args.td, ROTATION=args.tr)
+    fobj.set_tracking(HORIZONTAL=args.th, VERTICAL=args.tv, DISTANCE=args.td, ROTATION=args.tr)
 
     while True:
         try:
             img = tello.get_frame()
-
             # wait for valid frame
             if img is None: continue
 
             imghud = img.copy()
-            
             k = cv2.waitKey(pspeed)
-            
             if is_tracking:
                 fobj.set_image_to_process(img)
                 fobj.draw_detections(imghud, ANONIMUS=False)
-                cv2.imshow("TelloCamera",imghud)
+                cv2.imshow("TelloCamera", imghud)
 
         except cv2.error as e:
             print(f"OpenCV error: {e}")
@@ -201,32 +222,32 @@ if __name__=="__main__":
             break
 
         # write video
-        if k ==ord('v'):
-            if writevideo == False : writevideo = True
+        if k == ord('v'):
+            if not writevideo: writevideo = True
             else: writevideo = False
 
-        if writevideo == True:
+        if writevideo:
             videow.write(img)
 
         # exit
         if k == ord('q'):
             tello.stop_communication()
             break
-        
+
         if k == ord('t'):
             print("")
             tello.send_cmd('takeoff')
-            
+
         if k == ord('x'):
             print("X is pressed")
-            if is_tracking==False:
+            if not is_tracking:
                 is_tracking = True
                 print("Enabled Tracking")
-            elif is_tracking==True:
+            elif is_tracking:
                 is_tracking = False
                 print("Disabled Tracking")
-        
-        if k==ord('f'):
+
+        if k == ord('f'):
             print("Start listening...")
             command = get_command(mic, recogniser)
             command = translate(command)
